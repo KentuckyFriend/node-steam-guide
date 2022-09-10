3fd63f7b38161e22ee211d196e83886772bb7e9f
Tiitus Heikkinen <38552110+KentuckyFriend@users.noreply.github.com>
2022-09-10 19:11:22 +0300

    Delete Chapter 6 - Connecting Sites and Bots/Chapter 6.2 - Getting Started directory

--- Chapter 6 - Connecting Sites and Bots/Chapter 6.2 - Getting Started/project10/models/price.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model('Price', {
-  market_hash_name: String,
-  price: Number
-});
--- Chapter 6 - Connecting Sites and Bots/Chapter 6.2 - Getting Started/project10/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model('User', {
-  steamid: String,
-  personaname: String,
-  avatar: String,
-  avatarfull: String
-});
